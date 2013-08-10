a56f3a8d84607a3c1c02e0a3c7b785452925ab9c
donalquirke <donal.quirke@gmail.com>
2013-08-10 16:33:33 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Developmindfulness::Application.config.secret_token = 'd39f3a9f69c37807af3d609d20ff067f2385ca2809232842524677ad5c080e99ebbdb27007a1d01527637f1a4e9022f17b4037df43a12807acefe4f872d28095'
